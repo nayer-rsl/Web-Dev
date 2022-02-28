c2aeda56c57457413ad78dba312747dd7f59ecaa
nayer-rsl <nayer.rsl@gmail.com>
2022-02-28 16:27:16 -0500

    edited Countru view

---
+++ Semester2/5212-Php_C#/C#/Assignments/Travel-Application/PassionSem/PassionSem/Models/Document.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PassionSem.Models
+{
+    public class Document
+    {
+        [Key]
+        public int DocumentID { get; set; }
+        public string DocumentName { get; set; }
+
+        public ICollection<Country> Countries { get; set; }
+
+    }
+    public class DocumentDto
+    {
+        public int DocumentID { get; set; }
+        public string DocumentName { get; set; }
+    }
+
+}
